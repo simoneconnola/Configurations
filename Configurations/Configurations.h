13ba4ee52d565725abfe15938d4a2d007f52122b
Simone C <connola.simone@gmail.com>
2018-11-16 14:08:07 +0100

    Initial Commit

---
+++ Configurations/Configurations.h
@@ -0,0 +1,19 @@
+//
+//  Configurations.h
+//  Configurations
+//
+//  Created by Simone Connola on 16/11/2018.
+//  Copyright © 2018 Simone Connola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Configurations.
+FOUNDATION_EXPORT double ConfigurationsVersionNumber;
+
+//! Project version string for Configurations.
+FOUNDATION_EXPORT const unsigned char ConfigurationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Configurations/PublicHeader.h>
+
+
